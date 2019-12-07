6cf4972fcaaaae1260e3dd805f57ef0e584a9b9b
Eikthyrnir <timbarvenov@gmail.com>
2019-12-07 19:47:10 +0300

    now is Package-private as used only for services

--- src/main/java/data/service/ConnectionPool.java
+++ src/main/java/data/service/ConnectionPool.java
@@ -1,4 +1,4 @@
-package data;
+package data.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.sql.*;
 import java.util.Properties;
 
-public enum ConnectionPool {
+enum ConnectionPool {
 
     INSTANCE;
 
